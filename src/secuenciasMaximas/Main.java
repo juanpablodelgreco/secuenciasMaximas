89da13017671e23b49ca15fbf4f16e248e4a4713
juanpablodelgreco <juanpablo.delgreco@hotmail.com>
2020-04-23 12:37:19 -0300

    Ejercicio finalizado
    Se agrego el caso de fatiga.

--- src/secuenciasMaximas/Main.java
+++ src/secuenciasMaximas/Main.java
@@ -1,10 +1,30 @@
 package secuenciasMaximas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int numero = 4;
+		int numero = 7;
 		SecuenciaMaxima sm = new SecuenciaMaxima("./lote_de_pruebas/Inputs/"+numero+".in", "./lote_de_pruebas/Outputs/"+numero+".out");
 		sm.obtenerSecuenciaMaxima();
+	}
+	
+	public static void casoFatiga() {
+		String path = "./lote_de_pruebas/Inputs/7.in";
+		try {
+			PrintWriter pw = new PrintWriter(new File(path));
+			pw.println(1000);
+			for(int i=1; i<=1000; i++) {
+				pw.println(i);
+			}
+			System.out.println(path+" generado con exito!");
+			pw.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo crear el archivo "+path);
+		}
+	
 	}
 }
